9fa24b17e72838811c02cba280c8889bbec5941b
RicardoVaranda <ricardo.varanda@mycit.ie>
2015-02-17 16:46:32 +0000

    Elective preview Template
    I have created a elective preview and also shown how you can go about
    using it

---
+++ app/views/layout/electivepreview.blade.php
@@ -0,0 +1,37 @@
+<li class="mix {{ $mod->department->faculty->short() }}"> 
+	<a data-toggle="modal" role="button" href="#{{$mod->mcode}}"> <img src="images/portfolio/portfolio1.jpg" alt="portfolio">
+		<div><span>{{ $mod->mshorttitle }}</span></div>
+	</a>
+	<div class="modal fade" id="{{$mod->mcode}}" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-dialog">
+		  	<div class="modal-content">
+				<div class="modal-header">
+			  		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			  		<h3 class="h3 modal-title">{{ $mod->mfulltitle }}</h3>
+				</div>
+				<div class="modal-body">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-8">
+								<img src="images/portfolio/portfolio1.jpg" class="img-responsive" alt="portfolio">
+							</div>
+							<div class="col-sm-4">
+								<div class="modal-entery">
+									<p>Add a description here</p>
+									<h4 class="h4">Lecturer</h4>
+									{{ $mod->mcoordinator }}
+									<h4 class="h4">Field of Study</h4>
+									{{ $mod->mfieldofstudy }}
+									<h4 class="h4">Module Level</h4>
+									{{ $mod->mlevel }}
+									<h4 class="h4">Module Credits</h4>
+									{{ $mod->mcredits }}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+		  	</div>
+		</div>
+	</div>
+</li>
--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -159,43 +159,7 @@
 						</div>
 						<ul id="grid">
 							@foreach(Modules::where('melective', 1)->get() as $mod)
-								<li class="mix {{ $mod->department->faculty->short() }}"> 
-									<a data-toggle="modal" role="button" href="#{{$mod->mcode}}"> <img src="images/portfolio/portfolio1.jpg" alt="portfolio">
-									<div><span>{{ $mod->mshorttitle }}</span></div>
-									</a>
-									<div class="modal fade" id="{{$mod->mcode}}" tabindex="-1" role="dialog" aria-hidden="true">
-										<div class="modal-dialog">
-										  	<div class="modal-content">
-												<div class="modal-header">
-											  		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-											  		<h3 class="h3 modal-title">{{ $mod->mfulltitle }}</h3>
-												</div>
-												<div class="modal-body">
-													<div class="container">
-														<div class="row">
-															<div class="col-sm-8">
-																<img src="images/portfolio/portfolio1.jpg" class="img-responsive" alt="portfolio">
-															</div>
-															<div class="col-sm-4">
-																<div class="modal-entery">
-																	<p>Add a description here</p>
-																	<h4 class="h4">Lecturer</h4>
-																	{{ $mod->mcoordinator }}
-																	<h4 class="h4">Field of Study</h4>
-																	{{ $mod->mfieldofstudy }}
-																	<h4 class="h4">Module Level</h4>
-																	{{ $mod->mlevel }}
-																	<h4 class="h4">Module Credits</h4>
-																	{{ $mod->mcredits }}
-																</div>
-															</div>
-														</div>
-													</div>
-												</div>
-										  	</div>
-										</div>
-									</div>
-								</li>
+								@include('layout.electivepreview', array('mod' => $mod))
 							@endforeach
 							<li class="gap"></li>
 							<!-- "gap" elements fill in the gaps in justified grid -->
