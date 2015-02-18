e72707dffbf90361d0f4dd91a85955f3018f00a0
RicardoVaranda <ricardo.varanda@mycit.ie>
2015-02-18 21:55:35 +0000

    New Database

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -171,8 +171,7 @@
 			
 			<section class="section about">
 				<div class="container">
-					<h1 class="h1">Test</h1>
-						
+					<h1 class="h1">My Profile</h1>						
 					<div class="row">
 					
 						<div class="col-sm-6 col-md-8 col-lg-8">
