a14be25a9f59dfb2982220ee9e0eb6baf10e1d4c
Ricardo Varanda <rickyv08@gmail.com>
2015-02-19 20:55:00 +0000

    Created new layouts look at the read me to figure out how the layouts are going to work from now on.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,14 +3,20 @@
 class HomeController extends BaseController {
 
 	public function home(){
-		if(Auth::check())
-			return View::make('home');
+		if(Auth::check()){
+			if(Auth::user()->rank == 1) 
+				return View::make('layout.lecturer');
+			elseif (Auth::user()->rank == 2) 
+				return View::make('layout.hod');
+			elseif (Auth::user()->rank == 3) 
+				return View::make('layout.tech');
+			else
+				return View::make('layout.student');
+		}
 		else
+		{
 			return Redirect::route('account-sign-in');
-	}
-
-	public function test(){
-		return View::make('test');
+		}
 	}
 
 }
--- app/views/test.blade.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head></head>
-<body>
-	<h1>Testing</h1>
-	@foreach(Modules::where('melective', 1)->get() as $mod)
-		{{$mod->mshorttitle}} belongs to department: {{ $mod->department->name() }}
-	@endforeach
-</body>
-<html>
