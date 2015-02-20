abe61113095121be3aa9a1692afa5056966e5b8e
Robert Serwin <robert.serwin@mycit.ie>
2015-02-20 11:24:24 +0000

    Fix windows

--- app/views/account/password.blade.php
+++ app/views/account/password.blade.php
@@ -1,24 +1,17 @@
-
-	<form action="{{ URL::route('account-change-password-post') }}" method="post">
-		
+<form action ="{{ URL::route('account-change-password-post') }}" method="post">
 		<div class="field">
-			Old Password: <input type="password" name="old_password">
-			@if($errors->has('old_password'))
+			Old password: <input type="password" name="old_password">
+			@if ($errors->has('old_password'))
 				{{ $errors->first('old_password') }}
 			@endif
 		</div>
 		<div class="field">
-			New Password: <input type="password" name="password">
-			@if($errors->has('password'))
+			New password: <input type = "password" name="password">
+			@if ($errors->has('password'))
 				{{ $errors->first('password') }}
 			@endif
 		</div>
 		<div class="field">
-			New Password again: <input type="password" name="password_again">
-			@if($errors->has('password_again'))
-				{{ $errors->first('password_again') }}
-			@endif
+			
 		</div>
-		<input type="submit" value="Change password">
-		{{Form::token()}}
-	</form>
+</form>
